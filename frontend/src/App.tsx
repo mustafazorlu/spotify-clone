49ae4f55c31e3e83c2be513e7a74210021c73e1f
Mustafa Zorlu <mzorlu3579@gmail.com>
2025-03-24 22:41:51 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+
+    return (
+        <>
+            <div>
+                <Button variant={'outline'}>
+                    This is a button
+                </Button>
+            </div>
+        </>
+    )
+}
+
+export default App
