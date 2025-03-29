ccfc7723ef856b895d3ada7bc4a5089439b27688
Mustafa Zorlu <mzorlu3579@gmail.com>
2025-03-29 23:59:40 +0300

    frontend: add music store using Zustand for managing albums and songs

---
+++ frontend/src/stores/useMusicStore.ts
@@ -0,0 +1,30 @@
+import { axiosInstance } from "@/lib/axios";
+import { create } from "zustand";
+
+export const useMusicStore = create((set) => ({
+   albums: [],
+   songs: [],
+   isLoading: false,
+   error: null,
+
+   fetchAlbum: async () => {
+      set({
+         isLoading: true,
+         error: null,
+      });
+
+      try {
+         const response = await axiosInstance.get("/albums");
+
+         set({
+            albums: response.data,
+         });
+      } catch (error: any) {
+         set({
+            error: error.response.data.message,
+         });
+      } finally {
+         set({ isLoading: false });
+      }
+   },
+}));
