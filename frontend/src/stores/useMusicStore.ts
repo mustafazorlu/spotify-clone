76aba8a14e70d763922eac11f3acc8f2ca089eee
Mustafa Zorlu <mzorlu3579@gmail.com>
2025-04-03 03:11:20 +0300

    stores

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -1,13 +1,26 @@
 import { axiosInstance } from "@/lib/axios";
+import { Album, Song } from "@/types";
 import { create } from "zustand";
 
-export const useMusicStore = create((set) => ({
+interface MusicStore {
+   songs: Song[];
+   albums: Album[];
+   isLoading: boolean;
+   error: string | null;
+   currentAlbum: Album | null;
+
+   fetchAlbums: () => Promise<void>;
+   fetchAlbumById: (id: string) => Promise<void>;
+}
+
+export const useMusicStore = create<MusicStore>((set) => ({
    albums: [],
    songs: [],
    isLoading: false,
    error: null,
+   currentAlbum: null,
 
-   fetchAlbum: async () => {
+   fetchAlbums: async () => {
       set({
          isLoading: true,
          error: null,
@@ -24,6 +37,17 @@ export const useMusicStore = create((set) => ({
             error: error.response.data.message,
          });
       } finally {
+         set({ isLoading: false });
+      }
+   },
+   fetchAlbumById: async (id) => {
+      set({ isLoading: true, error: null });
+      try {
+         const res = await axiosInstance.get(`/albums/${id}`);
+         set({ currentAlbum: res.data });
+      } catch (error : any) {
+         set({ isLoading: true, error: error.res.data.message });
+      }finally{
          set({ isLoading: false });
       }
    },
